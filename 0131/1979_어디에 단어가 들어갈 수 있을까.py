1862a0cffda74f1bcdb91e07f16395fdc038facf
seonjae.lee <jamie9@naver.com>
2024-02-02 10:21:57 +0900

    update 0131

--- 0131/1979_어디에 단어가 들어갈 수 있을까.py
+++ 0131/1979_어디에 단어가 들어갈 수 있을까.py
@@ -5,25 +5,29 @@ T = int(input())
 
 for test_case in range(1, T+1):
     N, K = map(int, input().split())
-    my_arr = [list(map(int, input().split())) for _ in range(N)]
+    my_arr = [list(map(int, input().split())) + [0] for _ in range(N)] + [[0]*(N+1)]
+    # 0을 패딩하기
     # print(my_arr)
+    N += 1
 
-    cnt = result = 0
-    for r in range(N):
-        for c in range(N):
-            if 1 in my_arr[r]:
+    # 가로, 세로로 연속한 1의 개수가 K인 것의 개수 구하기
+    ans = 0
+    for i in range(N):
+        cnt = 0
+        for j in range(N):
+            if my_arr[i][j]:
                 cnt += 1
-        if cnt == 3:
-            row_sum = col_sum = 0
-            if my_arr[r][c] == 1:
-                for k in range(3):
-                    if 0 <= c+k < N:
-                        row_sum += my_arr[r][c+k]
-                    if 0 <= r+k < N:
-                        col_sum += my_arr[r+k][c]
-            if row_sum == K:
-                result += 1
-            if col_sum == K:
-                result += 1
+            else:    # my_arr[i][j] ==0
+                if cnt == K:
+                    ans += 1
+                cnt = 0
 
-    print(f'#{test_case} {result}')
+        for k in range(N):
+            if my_arr[k][i]:
+                cnt += 1
+            else:    # my_arr[k][i] ==0
+                if cnt == K:
+                    ans += 1
+                cnt = 0
+
+    print(f'#{test_case} {ans}')
