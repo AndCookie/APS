eec4f6f817555c5cd1372de0dd8e1e7919595cf4
AndCookie <seonjae97@gmail.com>
2024-02-13 11:14:59 +0900

    update 0208

--- 0208/18226_종이붙이기.py
+++ 0208/18226_종이붙이기.py
@@ -1,8 +1,12 @@
-# import sys
-# sys.stdin = open('18282_input.txt')
+import sys
+sys.stdin = open('18226_input.txt')
 
 T = int(input())
-for test_case in range(1, T+1):
-    N = int(input())
+for test_case in range(1, T + 1):
+    N = int(input()) // 10
+    tape = [0] * (N + 1)
+    tape[0] = tape[1] = 1
+    for i in range(2, N + 1):
+        tape[i] = 2 * tape[i - 2] + tape[i - 1]
 
-    print(f'#{test_case} ')
+    print(f'#{test_case} {tape[N]}')
--- 0208/5432_쇠막대기 자르기.py
+++ 0208/5432_쇠막대기 자르기.py
@@ -1,16 +1,30 @@
 import sys
 sys.stdin = open('5432_input.txt')
 
+# 10799 쇠막대기
+'''
+2
+()(((()())(())()))(())
+(((()(()()))(())()))(()())
+'''
+
 T = int(input())
-for test_case in range(1, T+1):
-    arr = list(input())
-    print(arr)
-    length = len(arr)
-    my = [[] for _ in range(len(arr))]
-    print(my)
-    for i in range(1, length):
-        if arr[i-1] == '(' and arr[i] == ')':
-            my[0].append(arr[i-1])
-            my[0].append(arr[i])
-    print(my)
-    print(f'#{test_case}')
+
+for test_case in range(1, T + 1):
+    my_arr = input()
+    my_arr = my_arr.replace("()", '0')
+    stack = []
+    ans = 0
+
+    for i in range(len(my_arr)):
+        if my_arr[i] == '(':
+            stack.append(my_arr[i])
+            ans += 1
+
+        elif my_arr[i] == '0':
+            ans += len(stack)
+
+        elif my_arr[i] == ')':
+            stack.pop()
+
+    print(f'#{test_case} {ans}')
