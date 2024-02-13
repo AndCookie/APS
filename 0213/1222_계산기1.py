7a5e3304f2b304e55dfe37523db7866792f9afc8
AndCookie <seonjae97@gmail.com>
2024-02-13 17:57:40 +0900

    update 0213

--- 0213/1222_계산기1.py
+++ 0213/1222_계산기1.py
@@ -2,17 +2,23 @@ import sys
 sys.stdin = open('1222_input.txt')
 
 
+# for test_case in range(1, 11):
+#     N = int(input()) # 문자열 계산식의 길이
+#     exp = list(input().split('+'))
+#     total = 0
+#     for e in exp:
+#         total += int(e)
+#     print(f'#{test_case} {total}')
+
 for test_case in range(1, 11):
     N = int(input()) # 문자열 계산식의 길이
-    exp = input()
-    stack = []
+    exp = list(input())
+    stack = [0]
     for e in exp:
         if e.isdecimal():
-            stack.append(e)
+            stack.append(int(e))
         else:
-            while len(stack)!=1 :
-                x = stack.pop()
-                y = stack.pop()
-                stack.append(x+y)
-
-    print(f'#{test_case} {stack[-1]}')
+            x = stack.pop()
+            y = stack.pop()
+            stack.append(x+y)
+    print(f'#{test_case} {stack[0]+stack[-1]}')
