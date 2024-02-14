4b6c9240480759d8b9a9b3f3aa4bcca25593947a
AndCookie <seonjae97@gmail.com>
2024-02-14 09:04:20 +0900

    update 0214

--- 0213/18384_미로.py
+++ 0213/18384_미로.py
@@ -1,32 +1,25 @@
 import sys
 sys.stdin = open('18384_input.txt')
 
+move = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+
 # 출발지점 2를 찾기
 def search(i, j):
-    result = 0
-    while True:
-        cnt = 0
-        for di, dj in [[0, 1], [1, 0], [0, -1], [-1, 0]]:
+    stack = [(i,j)]
+    maze[i][j] = 1
+    while stack:
+        i, j = stack.pop()
+        for di, dj in move:
             ni, nj = i + di, j + dj
             # print(ni, nj)
             if 0 <= ni < n and 0 <= nj < n:
-                if maze[ni][nj] == '1':
-                    cnt += 1
-                    # print(cnt)
-                    continue
-                elif maze[ni][nj] == '2':
+                if maze[ni][nj] == '2':
                     return 1
                 elif maze[ni][nj] == '0':
-                    i = ni
-                    j = nj
-                    maze[ni][nj] = 1
-                    cnt = 0
-                    # print()
-                    break
-            else:
-                cnt +=1
-        if cnt >= 3:
-            return 0
+                    stack.append((ni,nj))
+                    maze[ni][nj] = '1'
+    return 0
+
 
 T = int(input())
 for test_case in range(1, T + 1):
@@ -35,10 +28,10 @@ for test_case in range(1, T + 1):
     [maze.append(list(input())) for _ in range(n)]
     # print(maze) # [['1', '3', '1', '0', '1'], ['1', '0', '1', '0', '1']... ]
 
+    print(f'#{test_case}', end=' ')
+
     # 도착지점 3의 인덱스
     for i in range(n):
         for j in range(n):
             if maze[i][j] == '3':
-                ans = search(i, j)
-
-    print(f'#{test_case} {ans}')
+                print(search(i,j))
--- 0213/18404_부분집합.py
+++ 0213/18404_부분집합.py
@@ -2,28 +2,25 @@ import sys
 sys.stdin = open('18404_input.txt', encoding='utf-8')
 
 def f(i, k):
-    if i==k:
-        total = 0
+    total = 0
+    global cnt
+    if i == k:
         for j in range(k):
             if bit[j]:
-                total += nums[j]
+                total += arr[j]
         if total == 10:
-            return 1
-        else:
-            return 0
+            cnt += 1
     else:
-        # for j in range(2):
-        #	bit[i] = j
-        #	f(i+1, k)
         bit[i] = 1
         f(i+1, k)
         bit[i] = 0
         f(i+1, k)
+    return cnt
 
+arr = list(map(int, input().split()))
+N = len(arr)
+bit = [0]*N    # bit[i] : arr[i]가 부분집합에 포함되었는지 나타내는 배열
+cnt = 0
+ans = f(0, N) # bit[i]에 1또는0을 채우고, N개의 원소가 결정되면 부분집합을 출력
 
-nums = list(map(int, input().split()))
-N = len(nums)
-bit = [0]*N   # bit[i] : arr[i]가 부분집합에 포함되었는지 나타내는 배열
-ans = 0
-ans += f(0, N)       # bit[i]에 1또는0을 채우고, N개의 원소가 결정되면 부분집합을 출력
-print(ans)
+print(f'#1 {ans}')
